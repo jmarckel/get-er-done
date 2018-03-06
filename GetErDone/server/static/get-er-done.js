4a71c638db1df4b470e84bac4133ff869876c861
Jeff Marckel <jbmarckel@gmail.com>
2018-03-05 23:17:57 -0600

    auth0 auth working on client side!

--- GetErDone/server/static/get-er-done.js
+++ GetErDone/server/static/get-er-done.js
@@ -2,7 +2,6 @@ $(function(){
 
     var GetErDoneTask = Backbone.Model.extend({
 
-	// default values for an empty task
 	defaults: function() {
 	    return {
 		title: "empty task...",
@@ -188,5 +187,100 @@ $(function(){
 
 
     var App = new AppView;
+
+
+    // AUTH0
+
+
+    var content = $('.content');
+    var loadingSpinner = $('#loading');
+    content.css('display', 'block');
+    loadingSpinner.css('display', 'none');;
+
+    var webAuth = new auth0.WebAuth({
+        domain: 'techex-epoxyloaf-com.auth0.com',
+        clientID: 'JmEarsTOZ3uy04TxiGb4TK9Xu9cBJVaR',
+        redirectUri: 'http://techex.epoxyloaf.com/',
+        audience: 'https://techex-epoxyloaf-com.auth0.com/userinfo',
+        responseType: 'token id_token',
+        scope: 'openid',
+        leeway: 60
+    });
+
+    var loginStatus = $('.container h4');
+    var loginView = $('#login-view');
+    var homeView = $('#home-view');
+
+    // buttons and event listeners
+    var homeViewBtn = $('#btn-home-view');
+    var loginBtn = $('#btn-login');
+    var logoutBtn = $('#btn-logout');
+
+    homeViewBtn.click(function() {
+        homeView.css('display', 'inline-block');
+        loginView.css('display', 'none');
+    });
+
+    loginBtn.click(function(e) {
+        e.preventDefault();
+        webAuth.authorize();
+    });
+
+    logoutBtn.click(logout);
+
+    function setSession(authResult) {
+        // Set the time that the access token will expire at
+        var expiresAt = JSON.stringify(
+            authResult.expiresIn * 1000 + new Date().getTime()
+        );
+        localStorage.setItem('access_token', authResult.accessToken);
+        localStorage.setItem('id_token', authResult.idToken);
+        localStorage.setItem('expires_at', expiresAt);
+    }
+
+    function logout() {
+        // Remove tokens and expiry time from localStorage
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('id_token');
+        localStorage.removeItem('expires_at');
+        displayButtons();
+    }
+
+    function isAuthenticated() {
+        // Check whether the current time is past the
+        // access token's expiry time
+        var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+        return new Date().getTime() < expiresAt;
+    }
+
+    function handleAuthentication() {
+        webAuth.parseHash(function(err, authResult) {
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                window.location.hash = '';
+                setSession(authResult);
+                loginBtn.css('display', 'none');
+                homeView.css('display', 'inline-block');
+            } else if (err) {
+                homeView.css('display', 'inline-block');
+                console.log(err);
+                alert('Error: ' + err.error + '. Check the console for further details.');
+            }
+            displayButtons();
+        });
+    }
+
+    function displayButtons() {
+        if (isAuthenticated()) {
+            loginBtn.css('display', 'none');
+            logoutBtn.css('display', 'inline-block');
+            loginStatus.text('You are logged in!');
+        } else {
+            loginBtn.css('display', 'inline-block');
+            logoutBtn.css('display', 'none');
+            loginStatus.text('You are not logged in! Please log in to continue.');
+        }
+    }
+
+    handleAuthentication();
 
 });
